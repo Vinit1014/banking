dfd482a41c46f014e93c12a6e53e27fd10925605
Vinit <vinits1014@gmail.com>
2024-10-18 01:26:37 +0530

    Done with some parts of middle section of web app

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
+    const loggedIn = { firstName: 'Vinit' };
 
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <section className="home">
+        <div className="home-content">
+            <header className="home-header">
+            <HeaderBox 
+                type="greeting"
+                title="Welcome"
+                user={loggedIn?.firstName || 'Guest'}
+                subtext="Access and manage your account and transactions efficiently."
+            />
+
+            <TotalBalanceBox 
+                accounts={[]}
+                totalBanks={1}
+                totalCurrentBalance={1250.35}
+            />
+            </header>
+        </div>
+        </section>
+    )
 }
 
 export default Home
