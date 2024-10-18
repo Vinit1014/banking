4e7de86ce8fa56c6f5094d13d41881d6b3f37df0
Vinit <vinits1014@gmail.com>
2024-10-18 22:56:27 +0530

    Completed sidebars

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,26 +1,30 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSideBar from '@/components/RightSideBar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
     const loggedIn = { firstName: 'Vinit' };
 
     return (
         <section className="home">
-        <div className="home-content">
-            <header className="home-header">
-            <HeaderBox 
-                type="greeting"
-                title="Welcome"
-                user={loggedIn?.firstName || 'Guest'}
-                subtext="Access and manage your account and transactions efficiently."
-            />
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox 
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext="Access and manage your account and transactions efficiently."
+                    />
 
-            <TotalBalanceBox 
-                accounts={[]}
-                totalBanks={1}
-                totalCurrentBalance={1250.35}
-            />
-            </header>
-        </div>
+                    <TotalBalanceBox 
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1250.35}
+                    />
+                </header>
+                RECENT TRANSACTIONS
+            </div>
+
+            <RightSideBar user={loggedIn} transactions={[]} banks={[{ currentBalance: 123.50 },{ currentBalance: 123.50 }]}/>
         </section>
     )
 }
